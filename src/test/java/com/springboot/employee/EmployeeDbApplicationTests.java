54ccbffc0295b7810963a9b6088112dc9fe11993
Jaswant Reddy <reddyjaswant0199@gmail.com>
2022-12-26 20:05:56 +0530

    second commit

--- src/test/java/com/springboot/employee/EmployeeDbApplicationTests.java
+++ src/test/java/com/springboot/employee/EmployeeDbApplicationTests.java
@@ -1,4 +1,4 @@
-package com.luv2code.springboot.thymeleafdemo;
+package com.springboot.employee;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ThymeleafdemoApplicationTests {
+public class EmployeeDbApplicationTests {
 
 	@Test
 	public void contextLoads() {
